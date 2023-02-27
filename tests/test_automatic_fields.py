d4dfd994a93d0ede32e38abd50253545e71c1da1
RushiT0122 <rtodkar@stitchdata-talend.com>
2023-02-27 14:43:14 +0000

    Update the integration tests

---
+++ tests/test_automatic_fields.py
@@ -0,0 +1,83 @@
+import json
+
+from tap_tester import connections, runner, LOGGER
+
+from base import IterableBase
+
+class AutomaticFieldsTest(IterableBase):
+
+    def name(self):
+        return "tap_tester_iterable_bookmark_test"
+
+    def test_name(self):
+        LOGGER.info("Automatic Field Test for tap-iterable")
+
+    def test_run(self):
+        """
+        Verify we can deselect all fields except when inclusion=automatic, which is handled by base.py methods
+        Verify that only the automatic fields are sent to the target.
+        Verify that all replicated records have unique primary key values.
+        """
+        # instantiate connection
+        conn_id = connections.ensure_connection(self)
+
+        streams_to_test = self.expected_streams()
+
+        # run check mode
+        found_catalogs = self.run_and_verify_check_mode(conn_id)
+
+        # table and field selection
+        test_catalogs_automatic_fields = [catalog for catalog in found_catalogs
+                                          if catalog.get('stream_name') in streams_to_test]
+
+        self.perform_and_verify_table_and_field_selection(
+             conn_id, test_catalogs_automatic_fields, select_all_fields=False)
+
+        # run initial sync
+        record_count_by_stream = self.run_and_verify_sync(conn_id)
+        all_messages = runner.get_records_from_target_output()
+
+        for stream in streams_to_test:
+            with self.subTest(stream=stream):
+
+                # expected values
+                expected_primary_keys = self.expected_primary_keys()
+                expected_keys = self.expected_automatic_fields().get(stream)
+
+                # workaround for TDL-16245 , remove after bug fix
+                expected_keys = expected_keys - self.expected_replication_keys().get(stream)
+
+                # collect actual values
+                stream_messages = all_messages.get(stream)
+                record_messages_keys = [set(message['data'].keys())
+                                        for message in stream_messages['messages']
+                                        if  message['action'] == 'upsert']
+
+
+                # Verify that you get some records for each stream
+                self.assertGreater(
+                    record_count_by_stream.get(stream, -1), 0,
+                    msg="The number of records is not over the stream max limit")
+
+                # Verify that only the automatic fields are sent to the target
+                for actual_keys in record_messages_keys:
+                    self.assertSetEqual(expected_keys, actual_keys)
+
+                # Get records
+                records = [message.get("data") for message in stream_messages.get('messages', [])
+                            if message.get('action') == 'upsert']
+
+                # Remove duplicate records
+                records_pks_list = [tuple(message.get(pk) for pk in expected_primary_keys[stream])
+                                    for message in [json.loads(t) for t in {json.dumps(d) for d in records}]]
+
+                # Remove duplicate primary keys
+                records_pks_set = set(records_pks_list)
+
+                # Verify there are no duplicate records
+                self.assertEqual(len(records), len(records_pks_set),
+                                        msg=f"{stream} contains duplicate records")
+
+                # Verify defined primary key is unique
+                self.assertEqual(len(records_pks_set), len(records_pks_list),
+                                        msg=f"{expected_primary_keys} are not unique primary keys for {stream} stream.")
--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1,17 +1,125 @@
-import unittest
-
+import re
 from tap_tester import menagerie, connections
-from tap_tester.base_suite_tests.discovery_test import DiscoveryTest
-
 from base import IterableBase
 
 
-class IterableDiscoveryTest(DiscoveryTest, IterableBase):
-    """Standard Discovery Test"""
+class IterableDiscoveryTest(IterableBase):
+    """
+    Test to verify that the discover mode and metadata conforms to standards.
+    """
 
-    @staticmethod
-    def name():
-        return "tt_iterable_discovery"
+    def name(self):
+        return "tap_tester_iterable_discovery_test"
 
-    def streams_to_test(self):
-        return set(self.expected_metadata().keys())
+    def test_run(self):
+        """
+        Testing that discovery creates the appropriate catalog with valid metadata.
+        - Verify number of actual streams discovered match expected
+        - Verify the stream names discovered were what we expect
+        - Verify stream names follow naming convention.
+          Streams should only have lowercase alphas and underscores
+        - Verify there is only 1 top level breadcrumb
+        - Verify replication key(s)
+        - Verify primary key(s)
+        - Verify that if there is a replication key we are doing INCREMENTAL otherwise FULL
+        - Verify the actual replication matches our expected replication method
+        - Verify that primary, replication keys are given the inclusion of automatic.
+        - Verify that all other fields have inclusion of available metadata.
+        """
+        streams_to_test = self.expected_streams()
+
+        conn_id = connections.ensure_connection(self)
+
+        found_catalogs = self.run_and_verify_check_mode(conn_id)
+
+        found_catalog_names = {c['tap_stream_id'] for c in found_catalogs}
+
+        # Verify number of actual streams discovered match expected
+        # Verify the stream names discovered were what we expect
+        expected_streams = streams_to_test
+        self.assertEqual(expected_streams, found_catalog_names)
+
+        # Verify stream names follow naming convention
+        # Streams should only have lowercase alphas and underscores
+        self.assertTrue(all([re.fullmatch(r"[a-z_]+",  name) for name in found_catalog_names]),
+                        msg="One or more streams don't follow standard naming")
+
+        for stream in streams_to_test:
+            with self.subTest(stream=stream):
+
+                # Verify ensure the catalog is found for a given stream
+                catalog = list([catalog for catalog in found_catalogs if catalog["stream_name"] == stream])[0]
+                self.assertIsNotNone(catalog)
+
+                # Collecting expected values
+                expected_primary_keys = self.expected_primary_keys()[stream]
+                expected_replication_keys = self.expected_replication_keys()[stream]
+                expected_replication_method = self.expected_replication_method()[stream]
+
+                # Add primary keys and replication keys in automatically replicated keys to check
+                expected_automatic_fields = expected_primary_keys | expected_replication_keys
+
+                # Collecting actual values...
+                schema_and_metadata = menagerie.get_annotated_schema(conn_id, catalog['stream_id'])
+                metadata = schema_and_metadata["metadata"]
+                stream_properties = [item for item in metadata if item.get("breadcrumb") == []]
+                actual_primary_keys = set(
+                    stream_properties[0].get("metadata",
+                                             {self.PRIMARY_KEYS: []}).get(self.PRIMARY_KEYS, [])
+                )
+                actual_replication_keys = {item for item
+                                           in set(stream_properties[0].get("metadata").get(self.REPLICATION_KEYS, []))
+                                           if item}
+                actual_automatic_fields = set(
+                    item.get("breadcrumb", ["properties", None])[1] for item in metadata
+                    if item.get("metadata").get("inclusion") == "automatic"
+                )
+                actual_replication_method = stream_properties[0].get(
+                    "metadata",
+                    {self.REPLICATION_METHOD: set()}
+                ).get(self.REPLICATION_METHOD)
+
+                ##########################################################################
+                # Metadata Assertions
+                ##########################################################################
+
+                actual_fields = []
+                for md_entry in metadata:
+                    if md_entry['breadcrumb'] != []:
+                        actual_fields.append(md_entry['breadcrumb'][1])
+                # Verify there are no duplicate metadata entries.
+                self.assertEqual(len(actual_fields), len(set(actual_fields)))
+
+                # Verify there is only 1 top level breadcrumb in metadata
+                self.assertTrue(len(stream_properties) == 1,
+                                msg="There is NOT only one top level breadcrumb for {}".format(stream) +
+                                "\nstream_properties | {}".format(stream_properties))
+
+                # Verify primary key(s) match expectations
+                self.assertSetEqual(expected_primary_keys, actual_primary_keys)
+
+                # Verify replication key(s) match expectations
+                self.assertSetEqual(expected_replication_keys, actual_replication_keys)
+
+                # Verify that if there is a replication key we are doing INCREMENTAL otherwise FULL
+                if expected_replication_keys:
+                    self.assertEqual(self.INCREMENTAL, actual_replication_method)
+                else:
+                    self.assertEqual(self.FULL_TABLE, actual_replication_method)
+
+                # Verify the replication method matches our expectations
+                self.assertEqual(expected_replication_method, actual_replication_method)
+
+                # Verify that primary keys and replication keys
+                # are given the inclusion of automatic in metadata.
+                self.assertSetEqual(expected_automatic_fields, actual_automatic_fields)
+
+                # Verify that all other fields have inclusion of available
+                # This assumes there are no unsupported fields for SaaS sources
+                self.assertTrue(
+                    all({item.get("metadata").get("inclusion") == "available"
+                         for item in metadata
+                         if item.get("breadcrumb", []) != []
+                         and item.get("breadcrumb", ["properties", None])[1]
+                         not in actual_automatic_fields}),
+                    msg="Not all non key properties are set to available in metadata")
