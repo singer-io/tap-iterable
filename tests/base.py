6ce187a95befac1e9d6601d4ed3f7ce1fa13bc9a
RushiT0122 <rtodkar@stitchdata-talend.com>
2023-02-27 14:43:14 +0000

    Update the integration tests

--- tests/base.py
+++ tests/base.py
@@ -1,229 +1,272 @@
-"""
-Setup expectations for test sub classes
-Run discovery for as a prerequisite for most tests
-"""
+from tap_tester import connections, menagerie, runner
 import os
+import unittest
 from datetime import datetime as dt
-from datetime import timedelta
+import time
 
-from tap_tester import connections, menagerie, runner, LOGGER
-from tap_tester.base_suite_tests.base_case import BaseCase
+from tap_tester import connections, menagerie, runner
 
-
-class IterableBase(BaseCase):
+class IterableBase(unittest.TestCase):
     """
-    Setup expectations for test sub classes.
-    Metadata describing streams.
-    A bunch of shared methods that are used in tap-tester tests.
-    Shared tap-specific methods (as needed).
+    Base class for tap.
     """
 
+    START_DATE = ""
+    PRIMARY_KEYS = "table-key-properties"
+    REPLICATION_METHOD = "forced-replication-method"
+    REPLICATION_KEYS = "valid-replication-keys"
+    FULL_TABLE = "FULL_TABLE"
+    INCREMENTAL = "INCREMENTAL"
+    OBEYS_START_DATE = "obey-start-date"
+    START_DATE_FORMAT = "%Y-%m-%dT00:00:00Z"
+    REPLICATION_DATE_FOMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+    BOOKMARK_FOMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
-    REPLICATION_KEY_FORMAT = "%Y-%m-%dT00:00:00.000000Z"
-    BOOKMARK_FORMAT = "%Y-%m-%d"
-    PAGE_SIZE = 100000
-    start_date = ""
+    first_start_date = '2015-03-25T00:00:00Z'
+    second_start_date = '2020-08-15T00:00:00Z'
 
-
-    @staticmethod
-    def tap_name():
-        """The name of the tap"""
+    def tap_name(self):
         return "tap-iterable"
 
+    def setUp(self):
+        required_env = {
+            "ITERABLE_API_KEY"
+        }
+        missing_envs = [v for v in required_env if not os.getenv(v)]
+        if missing_envs:
+            raise Exception("set " + ", ".join(missing_envs))
 
-    @staticmethod
-    def get_type():
-        """the expected url route ending"""
+    def get_type(self):
         return "platform.iterable"
 
-
-    def get_properties(self, original: bool = True):
-        """Configuration properties required for the tap."""
-
-        return_value = {
-            # added defualt value as there is no current activity
-            'start_date':'2015-03-25T00:00:00Z', # '2018-03-25T00:00:00Z' for faster test runs
-            'api_key':os.getenv('TAP_ITERABLE_API_KEY'),
-            "api_window_in_days": 10
+    def get_credentials(self):
+        """
+        Setting required credentials as environment variables.
+        """
+        return {
+            "api_key": os.getenv("ITERABLE_API_KEY")
         }
 
+    def get_properties(self, original: bool = True):
+        """
+        Setting required properties as environment variables.
+        """
+        return_value = {
+            'start_date':'2015-03-25T00:00:00Z', # '2018-03-25T00:00:00Z' for faster test runs
+            'api_key':os.getenv('ITERABLE_API_KEY'),
+            "api_window_in_days": 10
+        }
         if original:
             return return_value
 
-        if self.start_date:
-            return_value["start_date"] = self.start_date
+        # Reassign start date
+        return_value["start_date"] = self.START_DATE
         return return_value
 
-
-    @staticmethod
-    def get_credentials():
-        return {
-            'api_key':os.getenv('TAP_ITERABLE_API_KEY'),
-        }
-
-
     def expected_metadata(self):
-        """The expected streams and metadata about the streams"""
-        default_expectations = {
-            self.PRIMARY_KEYS: {"id"},
-            self.REPLICATION_METHOD: self.INCREMENTAL,
-            self.REPLICATION_KEYS: {"date_updated"},
-            self.RESPECTS_START_DATE: True,
-        }
-
+        """
+        Provides the expected metadata for each stream.
+        """
         return {
-            'channels': {
-                self.AUTOMATIC_FIELDS: {
-                    'id'
-                },
+            "campaigns": {
+                self.PRIMARY_KEYS: {"id"},
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"updatedAt"},
+                self.OBEYS_START_DATE: True
+            },
+            "channels": {
                 self.PRIMARY_KEYS: {"id"},
                 self.REPLICATION_METHOD: self.FULL_TABLE,
-                self.RESPECTS_START_DATE: True,
+                self.OBEYS_START_DATE: True
+            },
+            "email_complaint": {
+                self.PRIMARY_KEYS: set(),
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"createdAt"},
+                self.OBEYS_START_DATE: True
+            },
+            "email_bounce": {
+                self.PRIMARY_KEYS: set(),
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"createdAt"},
+                self.OBEYS_START_DATE: True
+            },
+            "email_click": {
+                self.PRIMARY_KEYS: set(),
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"createdAt"},
+                self.OBEYS_START_DATE: True
+            },
+            "email_open": {
+                self.PRIMARY_KEYS: set(),
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"createdAt"},
+                self.OBEYS_START_DATE: True
+            },
+            "email_send": {
+                self.PRIMARY_KEYS: set(),
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"createdAt"},
+                self.OBEYS_START_DATE: True
+            },
+            "email_send_skip": {
+                self.PRIMARY_KEYS: set(),
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"createdAt"},
+                self.OBEYS_START_DATE: True
+            },
+            "email_subscribe": {
+                self.PRIMARY_KEYS: set(),
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"createdAt"},
+                self.OBEYS_START_DATE: True
+            },
+            "email_unsubscribe": {
+                self.PRIMARY_KEYS: set(),
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"createdAt"},
+                self.OBEYS_START_DATE: True
+            },
+            "lists": {
+                self.PRIMARY_KEYS: {"id"},
+                self.REPLICATION_METHOD: self.FULL_TABLE,
+                self.OBEYS_START_DATE: True
+            },
+            "list_users": {
+                self.PRIMARY_KEYS: set(),
+                self.REPLICATION_METHOD: self.FULL_TABLE,
+                self.OBEYS_START_DATE: True
+            },
+            "message_types": {
+                self.PRIMARY_KEYS: {"id"},
+                self.REPLICATION_METHOD: self.FULL_TABLE,
+                self.OBEYS_START_DATE: True
+            },
+            "metadata": {
+                self.PRIMARY_KEYS: {"key"},
+                self.REPLICATION_METHOD: self.FULL_TABLE,
+                self.OBEYS_START_DATE: True
+            },
+            "templates": {
+                self.PRIMARY_KEYS: {"templateId"},
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"updatedAt"},
+                self.OBEYS_START_DATE: True
+            },
+            "users": {
+                self.PRIMARY_KEYS: set(),
+                self.REPLICATION_METHOD: self.INCREMENTAL,
+                self.REPLICATION_KEYS: {"createdAt"},
+                self.OBEYS_START_DATE: True
             }
         }
 
-    def expected_automatic_fields(self):
-        auto_fields = {}
-        for k, v in self.expected_metadata().items():
-            auto_fields[k] = v.get(self.AUTOMATIC_FIELDS, set())
-
-        return auto_fields
-
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass(logging="Ensuring environment variables are sourced.")
-        missing_envs = [
-            x for x in [
-                'TAP_ITERABLE_API_KEY' #TODO add account_id and password if needed(credentials)
-            ] if os.getenv(x) is None
-        ]
-
-        if len(missing_envs) != 0:
-            raise Exception("Missing environment variables: {}".format(missing_envs))
-
-
-    ##########################################################################
-    ### Tap Specific Methods
-    ##########################################################################
-
-
-    @staticmethod
-    def expected_pagination_fields(): # TODO does this apply?
-        return {
-            "Test Report 1" : set(),
-            "Audience Overview": {
-                "ga:users", "ga:newUsers", "ga:sessions", "ga:sessionsPerUser", "ga:pageviews",
-                "ga:pageviewsPerSession", "ga:sessionDuration", "ga:bounceRate", "ga:date",
-                # "ga:pageviews",
-            },
-            "Audience Geo Location": set(),
-            "Audience Technology": set(),
-            "Acquisition Overview": set(),
-            "Behavior Overview": set(),
-            "Ecommerce Overview": set(),
-        }
-
-
-
-    # TODO refactor code to remove this method if possible, ga4 relic
-    def get_stream_name(self, stream):
+    def expected_streams(self):
         """
-        Returns the stream_name given the tap_stream_id because synced_records
-        from the target output batches records by stream_name
-        Since the GA4 tap_stream_id is a UUID instead of the usual case of
-        tap_stream_id == stream_name, we need to get the stream_name that
-        maps to tap_stream_id
+        Returns expected streams for tap.
         """
-        # custom_reports_names_to_ids().get(tap_stream_id, tap_stream_id)
-        return self.stream
+        return set(self.expected_metadata().keys())
 
+    def expected_replication_keys(self):
+        """
+        Returns expected replication keys for streams in tap.
+        """
+        return {table: properties.get(self.REPLICATION_KEYS, set()) for table, properties
+                in self.expected_metadata().items()}
 
-    @staticmethod
-    def select_all_streams_and_fields(conn_id, catalogs, select_all_fields: bool = True):
+    def expected_primary_keys(self):
+        """
+        Returns expected primary keys for streams in tap.
+        """
+        return {table: properties.get(self.PRIMARY_KEYS, set()) for table, properties
+                in self.expected_metadata().items()}
+
+    def expected_replication_method(self):
+        """
+        Returns expected replication method for streams in tap.
+        """
+        return {table: properties.get(self.REPLICATION_METHOD, set()) for table, properties
+                in self.expected_metadata().items()}
+
+    def select_found_catalogs(self, conn_id, catalogs, only_streams=None,
+                              deselect_all_fields: bool = False, non_selected_props=[]):
         """Select all streams and all fields within streams"""
         for catalog in catalogs:
-            schema = menagerie.get_annotated_schema(conn_id, catalog['stream_id'])
+            if only_streams and catalog["stream_name"] not in only_streams:
+                continue
+            schema = menagerie.get_annotated_schema(conn_id, catalog["stream_id"])
 
-            non_selected_properties = []
-            if not select_all_fields:
-                # get a list of all properties so that none are selected
-                non_selected_properties = schema.get('annotated-schema', {}).get(
-                    'properties', {}).keys()
+            non_selected_properties = non_selected_props if not deselect_all_fields else []
+            if deselect_all_fields:
+                # Get a list of all properties so that none are selected
+                non_selected_properties = schema.get("annotated-schema", {}).get("properties", {})
+                non_selected_properties = non_selected_properties.keys()
+            additional_md = []
 
-            connections.select_catalog_and_fields_via_metadata(
-                conn_id, catalog, schema, [], non_selected_properties)
+            connections.select_catalog_and_fields_via_metadata(conn_id,
+                                                               catalog,
+                                                               schema,
+                                                               additional_md=additional_md,
+                                                               non_selected_fields=non_selected_properties)
 
-
-    def perform_and_verify_table_and_field_selection(self,
-                                                     conn_id,
-                                                     test_catalogs,
-                                                     select_all_fields=True):
+    def run_and_verify_check_mode(self, conn_id):
         """
-        Perform table and field selection based off of the streams to select
-        set and field selection parameters.
-        Verify this results in the expected streams selected and all or no
-        fields selected for those streams.
+        Run the tap in check mode and verify it succeeds.
+        This should be ran prior to field selection and initial sync.
+        Return the connection id and found catalogs from menagerie.
         """
+        # Run in check mode
+        check_job_name = runner.run_check_mode(self, conn_id)
 
+        # Verify check exit codes
+        exit_status = menagerie.get_exit_status(conn_id, check_job_name)
+        menagerie.verify_check_exit_status(self, exit_status, check_job_name)
 
-        # Select all available fields or select no fields from all testable streams
-        self.select_all_streams_and_fields(
-            conn_id=conn_id, catalogs=test_catalogs, select_all_fields=select_all_fields
+        found_catalogs = menagerie.get_catalogs(conn_id)
+        self.assertGreater(len(found_catalogs), 0,
+                           msg="unable to locate schemas for connection {}".format(conn_id))
+
+        found_catalog_names = set(map(lambda c: c['stream_name'], found_catalogs))
+        print(found_catalog_names)
+        self.assertSetEqual(set(self.expected_metadata().keys()), found_catalog_names,
+                            msg="discovered schemas do not match")
+        print("discovered schemas are OK")
+
+        return found_catalogs
+
+    def run_and_verify_sync(self, conn_id, streams=None):
+        """
+        Runs sync mode and verifies the exit status for the same.
+        """
+        sync_job_name = runner.run_sync_mode(self, conn_id)
+
+        # Verify tap and target exit codes
+        exit_status = menagerie.get_exit_status(conn_id, sync_job_name)
+        menagerie.verify_sync_exit_status(self, exit_status, sync_job_name)
+
+        sync_record_count = runner.examine_target_output_file(self,
+                                                              conn_id,
+                                                              streams if streams else self.expected_streams(),
+                                                              self.expected_primary_keys())
+
+        self.assertGreater(
+            sum(sync_record_count.values()), 0,
+            msg="failed to replicate any data: {}".format(sync_record_count)
         )
+        print("total replicated row count: {}".format(sum(sync_record_count.values())))
 
-        catalogs = menagerie.get_catalogs(conn_id)
+        return sync_record_count
 
-        # Ensure our selection affects the catalog
-        expected_selected = [tc.get('stream_name') for tc in test_catalogs]
-        for cat in catalogs:
-            catalog_entry = menagerie.get_annotated_schema(conn_id, cat['stream_id'])
-
-            # Verify all testable streams are selected
-            selected = catalog_entry.get('annotated-schema').get('selected')
-            print("Validating selection on {}: {}".format(cat['stream_name'], selected))
-            if cat['stream_name'] not in expected_selected:
-                self.assertFalse(selected, msg="Stream selected, but not testable.")
-                continue # Skip remaining assertions if we aren't selecting this stream
-            self.assertTrue(selected, msg="Stream not selected.")
-
-            if select_all_fields:
-                # Verify all fields within each selected stream are selected
-                for field, field_props in catalog_entry.get('annotated-schema').get('properties').items():
-                    field_selected = field_props.get('selected')
-                    print("\tValidating selection on {}.{}: {}".format(
-                        cat['stream_name'], field, field_selected))
-                    self.assertTrue(field_selected, msg="Field not selected.")
-            else:
-                # Verify only automatic fields are selected
-                expected_automatic_fields = self.expected_automatic_fields().get(cat['stream_name'])
-                selected_fields = self.get_selected_fields_from_metadata(catalog_entry['metadata'])
-                self.assertEqual(expected_automatic_fields, selected_fields)
-
-
-    def get_sync_start_time(self, stream, bookmark):
+    def dt_to_ts(self, dtime, format):
         """
-        Calculates the sync start time, with respect to the lookback window
+        Converts datetime to timestamp format.
         """
-        conversion_day = dt.now().replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=None) - timedelta(days=self.lookback_window)
-        bookmark_datetime = dt.strptime(bookmark, self.BOOKMARK_FORMAT)
-        start_date_datetime = dt.strptime(self.start_date, self.START_DATE_FORMAT)
-        return  min(bookmark_datetime, max(start_date_datetime, conversion_day))
+        date_stripped = int(time.mktime(dt.strptime(dtime, format).timetuple()))
+        return date_stripped
 
-
-    # TODO is this still useful now that we have get_stream_name?
-    def get_record_count_by_stream(self, record_count, stream):
-        count = record_count.get(stream)
-        if not count:
-            stream_name = self.custom_reports_names_to_ids().get(stream)
-            return record_count.get(stream_name)
-        return count
-
-
-    def get_bookmark_value(self, state, stream):
-        bookmark = state.get('bookmarks', {})
-        stream_bookmark = bookmark.get(stream)
-        stream_replication_key = self.expected_metadata().get(stream,set()).get('REPLICATION_KEYS')
-        if stream_bookmark:
-            return stream_bookmark.get(stream_replication_key)
-        return None
+    def is_incremental(self, stream):
+        """
+        Verifies if the stream provided is incremental or not and returns the corresponding
+        boolean result.
+        """
+        return self.expected_metadata()[stream][self.REPLICATION_METHOD] == self.INCREMENTAL
