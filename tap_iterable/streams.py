829760d556fd8b2267fe1099c6ea2ec5f6d0ffbb
Rutuja Deshmukh <rdeshmukh@talend.com>
2023-02-28 16:40:18 +0000

    Made replication keys as automatic during discovery

--- tap_iterable/streams.py
+++ tap_iterable/streams.py
@@ -94,7 +94,6 @@ class Stream():
         stream_metadata = metadata.to_list(stream_metadata)
         return stream_metadata
 
-
     # The main sync function.
     def sync(self, state):
         get_data = getattr(self.client, self.name)
