ed4a648af33e07b98a6515eb3b8474264903f921
Rutuja Deshmukh <rdeshmukh@talend.com>
2023-03-07 08:30:40 +0000

    added file in import

--- tap_iterable/sync.py
+++ tap_iterable/sync.py
@@ -7,6 +7,7 @@ import singer
 import singer.metrics as metrics
 from singer import metadata
 from singer import Transformer
+from tap_iterable.streams import STREAMS
 
 LOGGER = singer.get_logger()
 
