be620ff48bf8e1ae88c4831ea1040addac2987c8
shantanu73 <shantanudhiman25@gmail.com>
2023-03-07 15:27:23 +0000

    Changes: 1) Fixed the discover logic. 2) Modified load_metadata() method.

--- tap_iterable/discover.py
+++ tap_iterable/discover.py
@@ -9,109 +9,45 @@ from tap_iterable.streams import STREAMS
 def discover_streams(client):
   streams = []
 
-  for s in STREAMS.values():
-    s = s(client)
-    schema = singer.resolve_schema_references(s.load_schema())
+  for stream in STREAMS.values():
+    s = stream(client)
 
     # If stream is `users`, then get dynamic fields via API.
     if s.name == "users":
       res = s.client.get_user_fields()
-      fields = res["fields"]
-      # Merge `fields` with schema.
-      field_schema = translate_to_schema(fields)
-      schema = merge(schema, field_schema)
+      schema = convert_to_schema(res["fields"])
+    else:
+      schema = s.load_schema()
 
-    streams.append({'stream': s.name, 'tap_stream_id': s.name, 'schema': schema, 'metadata': s.load_metadata()})
+    streams.append({'stream': s.name, 'tap_stream_id': s.name, 'schema': schema, 'metadata': s.load_metadata(schema)})
   return streams
 
-#
-# Helper function to infer schema datatype.
-#
 
-def get_schema_datatype(v):
-  NUMBER_TYPES = set([
-    'double'
-  ])
-  INTEGER_TYPES = set([
-    'long'
-  ])
-  DATE_TYPES = set([
-    'date'
-  ])
-  STRING_TYPES = set([
-    'string'
-  ])
-  BOOLEAN_TYPES = set([
-    'boolean'
-  ])
+def get_field_type_schema(field_type):
+    if field_type == "boolean":
+        return {"type": ["null", "boolean"]}
 
-  dt = ""
+    if field_type == "date":
+        return {"type": ["null", "string"], "format": "date-time"}
 
-  # Check datatypes.
-  if v in NUMBER_TYPES:
-    dt = "number"
-  elif v in INTEGER_TYPES:
-    dt = "integer"
-  elif v in DATE_TYPES:
-    dt = "string"
-  elif v in STRING_TYPES:
-    dt = "string"
-  elif v in BOOLEAN_TYPES:
-    dt = "boolean"
+    if field_type == "long":
+      return {"type": ["null", "integer"]}
 
-  datatype = {
-    "type": [
-      "null",
-      dt
-    ]
-  }
+    if field_type == "double":
+        return {"type": ["null", "number"]}
 
-  if v in DATE_TYPES:
-    datatype["format"] = "date-time"
+    if field_type == "object":
+        return {"anyOf": [{"type": ["null", "object"]}, {"type": ["null", "array"], "items": {"type": ["null", "object"]}}]}
 
-  return datatype
-
-#
-# Helper function to translate `user.fields` => singer schema.
-#
-
-def translate_to_schema(fields):
-  schema = {
-    "properties": {}
-  }
-  for k, v in fields.items():
-    # If k has a ".", k is an object.
-    if "." in k:
-      # Create the object.
-      k_name = k.split(".")[0]
-      k_value = k.split(".")[1]
-
-      # If key not created, then create.
-      if k_name not in schema["properties"]:
-        schema["properties"][k_name] = { "type": ["null", "object"], "properties": {} }
-
-      schema["properties"][k_name]["properties"][k_value] = get_schema_datatype(v)
-
-    else:
-      if v != "object":
-        schema['properties'][k] = get_schema_datatype(v)
-
-  return schema
+    return {"type": ["null", "string"]}
 
 
-def merge(left, right):
-  """This is a deep merge implementation.
-  It's called on manifest_table dicts with the intent of merging all of
-  them into a superset of all of their contents. The tricky part is that
-  the values need to be merged as well."""
-  merged = left
+def convert_to_schema(fields):
+    schema = {
+      "type": ["null", "object"],
+      "properties": {}
+    }
+    for field_name, field_type in fields.items():
+        schema['properties'][field_name] = get_field_type_schema(field_type)
+    return schema
 
-  for table_key, table_value in right.items():
-    if left.get(table_key):
-      for key in table_value.keys():
-        if left[table_key].get(key):
-          merged[table_key][key] = merged[table_key][key] or table_value[key]
-    else:
-      merged[table_key] = right[table_key]
-
-  return merged
