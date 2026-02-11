9ae76abaf624dbd95f80f69bfbe3e828946c4bb5
satyendra101 <satyendra.singh@qlik.com>
2026-02-11 12:21:20 +0530

    SAC-30126 SAC-30248: Fix case-sensitive fields in users.json and schema issues

--- tap_iterable/sync.py
+++ tap_iterable/sync.py
@@ -7,6 +7,7 @@ import singer
 import singer.metrics as metrics
 from singer import metadata
 from singer import Transformer
+from tap_iterable.helper import transform_case_sensitive_fields
 from tap_iterable.streams import STREAMS
 
 LOGGER = singer.get_logger()
@@ -54,9 +55,12 @@ def sync_stream(state, instance):
     with metrics.record_counter(stream.tap_stream_id) as counter:
         for (stream, record) in instance.sync(state):
             counter.increment()
+            LOGGER.info(f" Record: {record}\n\n")
 
             with Transformer(integer_datetime_fmt="unix-milliseconds-integer-datetime-parsing") as transformer:
                 record = transformer.transform(record, stream.schema.to_dict(), metadata.to_map(stream.metadata))
+                if stream.tap_stream_id == "users":  # Specific check for users as it is causing validation issues
+                    record = transform_case_sensitive_fields(record=record)
                 singer.write_record(stream.tap_stream_id, record)
 
         if instance.replication_method == "INCREMENTAL":
